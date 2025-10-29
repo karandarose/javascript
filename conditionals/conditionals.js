77cded880e9b2f2701b205886c9d274e093e84bc
KaRanda Krompel <karandarose@gmail.com>
2025-10-29 17:38:09 -0600

    conditional assignment done

--- conditionals/conditionals.js
+++ conditionals/conditionals.js
@@ -1,3 +1,4 @@
+// 1
 const firstObject = {
   id: 1,
   name: "Ryan",
@@ -8,3 +9,29 @@ const secondObject = {
 };
 const mergeObject = Object.assign({}, firstObject, secondObject);
 console.log(mergeObject);
+
+// 2
+function largestNum(num1, num2) {
+  if (num1 > num2) {
+    console.log(num1);
+  } else {
+    console.log(num2);
+  }
+}
+largestNum(13, 12);
+
+// 3
+const sentence = "I am a sentence";
+const words = sentence.split(" ");
+console.log(words);
+const capitalizeWords = words.map((word) => {
+  console.log(word);
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+});
+console.log(capitalizeWords.join(" "));
+
+// 4
+const sentence = "Hi there";
+const sentences = sentence.toLowerCase();
+const words = sentences.replace(" ", "").split("").sort();
+console.log(words.join(""));
